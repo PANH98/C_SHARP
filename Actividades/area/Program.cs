7fe579ef4f81d5975ba8c215eff3e0f1451d5f68
Pedro Antonio Nava Hernández <panh98@hotmail.com>
2025-11-03 00:08:50 -0600

    Add files via upload

---
+++ Actividades/area/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+namespace AppAreaCirculo
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            double area;
+            int radio = 5;
+            const double pi = 3.1416;
+            area = pi * radio * radio;
+
+            Console.WriteLine(area);
+            Console.ReadKey();
+        }
+    }
+
+
+
+
+}

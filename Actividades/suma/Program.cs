26a47d68f698901ec1e729e4f0cb3bad6d78fbc5
Pedro Antonio Nava Hernández <panh98@hotmail.com>
2025-11-03 00:07:27 -0600

    Add files via upload

---
+++ Actividades/suma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumadecimales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double suma;
+            double num_uno = 34.6;
+            double num_dis = 65.4;
+
+            suma = num_uno + num_dis;
+
+            Console.WriteLine(suma);
+            Console.ReadKey();
+        }
+
+
+
+    }
+}

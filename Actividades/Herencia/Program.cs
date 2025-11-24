bda2d54b6e245531670723484b8b7660c52e3cdd
Pedro Antonio Nava Hernández <panh98@hotmail.com>
2025-11-23 23:38:10 -0600

    Add files via upload

---
+++ Actividades/Herencia/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Chihuahua perro1 = new Chihuahua();
+            Console.WriteLine(perro1.PerroLadrar());
+            Console.ReadKey();
+
+        }
+    
+    }
+    public class Perro
+    {
+        public string PerroLadrar() { return "Perro ladrando"; }
+    }
+
+    public class Chihuahua : Perro
+    { 
+    
+    public void ChihuahuaLadrar()
+        {
+            base.PerroLadrar();
+        }
+        
+
+
+    
+    
+    }
+
+
+
+
+
+
+
+
+}
